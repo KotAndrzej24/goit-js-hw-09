d29c114c6e08047e42e10b3c8c269cf58a3643c6
KotAndrzej24 <110996704+KotAndrzej24@users.noreply.github.com>
2023-01-16 07:52:12 +0000

    Deploying to gh-pages from @ KotAndrzej24/goit-js-hw-09@e690f2151a73dab3c085a8f95b65a4c98d946cfd 🚀

---
+++ 01-color-switcher.d1dd2ef4.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");t.disabled=!0;const a=()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`};e.addEventListener("click",(()=>{timer=setInterval(a,1e3),e.disabled=!0,t.disabled=!1}));t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(timer)}));
+//# sourceMappingURL=01-color-switcher.d1dd2ef4.js.map
